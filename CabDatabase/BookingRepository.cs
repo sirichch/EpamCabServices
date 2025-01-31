4116e150a4b03119a226c660aa83067d50617ea7
Chava Siri <chava_siri@epam.com>
2025-01-31 14:41:23 +0530

    added linked projects

---
+++ CabDatabase/BookingRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabServices;
+
+namespace CabDatabase
+{
+    public class BookingRepository
+    {
+        private List<Booking> _bookings;
+        public BookingRepository()
+        {
+            _bookings = new List<Booking>();
+        }
+        public void AddBooking(Booking booking)
+        {
+            _bookings.Add(booking);
+        }
+        public List<Booking> GetBookingsByUserId(int userId)
+        {
+            return _bookings.Where(b => b.UserId == userId).ToList();
+        }
+    }
+}
---
+++ CabDatabase/CabRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabServices;
+
+namespace CabDatabase
+{
+    public class CabRepository
+    {
+        private List<Cab> _cabs;
+        public CabRepository()
+        {
+            _cabs = new List<Cab>();
+        }
+        public void AddCab(Cab cab)
+        {
+            _cabs.Add(cab);
+        }
+        public List<Cab> GetAllCabs()
+        {
+            return _cabs;
+        }
+        public Cab GetCabById(int id)
+        {
+            return _cabs.FirstOrDefault(c => c.CabId == id);
+        }
+    }
+}
